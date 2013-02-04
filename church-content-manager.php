ca454b0dca3cbd8fa5e35352b0e735f468d83484
Steven Gliebe <work@stevengliebe.com>
2013-02-04 14:50:22 -0600

    Correct version

--- church-content-manager.php
+++ church-content-manager.php
@@ -6,7 +6,7 @@ Description: This plugin provides content management functionality for sermons, 
 Author: churchthemes.com
 Author URI: http://churchthemes.com
 License: GPL2
-Version: 0.5.0
+Version: 0.5
 
 Copyright 2012 DreamDolphin Media, LLC
 
