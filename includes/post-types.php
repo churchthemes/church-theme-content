d9eebc6c0718b9acee70540f1381e10a28c6cbc3
Steven Gliebe <work@stevengliebe.com>
2013-03-25 15:06:58 -0500

    Prevent pages from using post type rewrite slugs

--- includes/post-types.php
+++ includes/post-types.php
@@ -215,3 +215,37 @@ function ccm_register_post_type_person() {
 	);
 	
 }
+
+/**********************************
+ * RESERVED SLUGS
+ **********************************/ 
+
+/**
+ * Prevent post from using slug reserved for a post type archive
+ *
+ * If an identical slug is entered, it will be appended by a number.
+ * For example, a 'sermons' page slug will become 'sermons-2'.
+ *
+ * This is broad but mainly intended for top-level pages.
+ */
+
+add_filter( 'wp_unique_post_slug_is_bad_flat_slug', 'ccm_is_bad_post_slug', 10, 2 );
+add_filter( 'wp_unique_post_slug_is_bad_hierarchical_slug', 'ccm_is_bad_post_slug', 10, 2 );
+
+function ccm_is_bad_post_slug( $current_value, $slug ) {
+
+	// Get post types with archives
+	$post_types = get_post_types( array(
+		'has_archive' => true
+	), 'objects' );
+
+	// Check if post slug matches a post type rewrite slug
+	foreach( $post_types as $post_type ) {
+		if ( ! empty( $post_type->rewrite['slug'] ) && $post_type->rewrite['slug'] == $slug ) {
+			return true;
+		}
+	}
+
+	return $current_value;
+
+}
