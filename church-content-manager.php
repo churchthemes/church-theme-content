7a15c16febba992552ba59d81f7638983b051d51
Steven Gliebe <work@stevengliebe.com>
2013-06-07 13:35:38 -0500

    Add upload mime types for sermon audio and video

--- church-content-manager.php
+++ church-content-manager.php
@@ -6,22 +6,7 @@
  * Author: churchthemes.com
  * Author URI: http://churchthemes.com
  * Version: 0.8.5
- * License: GPL2
- * 
- * Copyright 2012 - 2013 DreamDolphin Media, LLC
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as 
- * published by the Free Software Foundation.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * License: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * @package   Church_Content_Manager
  * @copyright Copyright (c) 2013, churchthemes.com
--- includes/mime-types.php
+++ includes/mime-types.php
@@ -10,20 +10,63 @@
  * @since      0.9
  */
 
-/**
- * Add Mime Types
- *
- * Add mime types to the array provided by WordPress core (image, audio, video).
- * 
- * This makes it easier to manage sermon PDF's in media manager.
- * 'application/pdf' can also be used as upload_type in sermon's PDF field (as 'audio' is for MP3).
- */
-
 // No direct access
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
- * Add mime types
+ * Add mime types for media upload
+ *
+ * The sermon feature supports audio, video and PDF. Make sure these files can be uploaded.
+ * The audio and video types below are supported by the MediaElement.js core WordPress player.
+ *
+ * See wp_get_mime_types() for available mime types.
+ *
+ * @since 0.9
+ * @param array $mime_types Currently uploadable mime types
+ * @return array Mime types with additions
+ */
+
+function ccm_add_mime_types( $mime_types ) {
+
+	// Sermon feature supported?
+	if ( ccm_feature_supported( 'sermons' ) ) {
+
+		// Video
+		$mime_types['webm']			= 'video/webm';
+		$mime_types['ogv']			= 'video/ogg';
+		$mime_types['mp4|m4v']		= 'video/mp4';
+		$mime_types['wmv']			= 'video/x-ms-wmv';
+		$mime_types['mov|qt']		= 'video/quicktime';
+		$mime_types['flv']			= 'video/x-flv';
+
+		// Audio
+		$mime_types['mp3|m4a|m4b']	= 'audio/mpeg';
+		$mime_types['ogg|oga']		= 'audio/ogg';
+		$mime_types['wma']			= 'audio/x-ms-wma';
+		$mime_types['wav']			= 'audio/wav';
+
+		// PDF
+		$mime_types['pdf']			= 'application/pdf';
+
+	}
+
+	return $mime_types;
+
+}
+
+add_filter( 'upload_mimes', 'ccm_add_mime_types' );
+
+/**
+ * Add mime types for media filtering
+ * 
+ * The media manager will have a new PDF filter in addition to the default Image, Video and Audio options.
+ * This makes it easier to manage sermon PDF's
+ * 
+ * 'application/pdf' can also then be used as upload_type in sermon's PDF field (as 'audio' is for MP3).
+ *
+ * @since  0.9
+ * @param array $post_mime_types Default mime types
+ * @return array Modified mime types
  */
 function ccm_add_post_mime_types( $post_mime_types ) {
 
