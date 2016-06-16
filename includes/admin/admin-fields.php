22c6182d7bab7e16acc862a4bd4341d0cb27e944
Steven Gliebe <work@stevengliebe.com>
2016-06-16 14:20:49 -0500

    Finish 'Get From Address' button geocoding via Google Maps API geocoder

--- includes/admin/admin-fields.php
+++ includes/admin/admin-fields.php
@@ -43,9 +43,9 @@ function ctc_coordinate_field( $data ) {
 add_action( 'admin_init', 'ctc_hide_theme_support_notice' ); // before admin_notices
 
 /**
- * Process "Get From Address"
+ * Process "Get From Address" button click
  *
- * Fill latitude/longitude fields when button clicked.
+ * Fill latitude/longitude inputs when button clicked.
  * Uses Google Maps geocoding service.
  *
  * @since 1.6
@@ -91,48 +91,69 @@ function ctc_get_coordinates_js() {
 	$error_failed = __( 'Latitide and Longitude could not be determined from Address. Please check for errors or enter manually.', 'church-theme-content' );
 
 	// JavaScript for click on "Get From Address" button
-?>
+	?>
 
-<script type="text/javascript">
+	<script type="text/javascript">
 
-jQuery( document ).ready( function( $ ) {
+	jQuery( document ).ready( function( $ ) {
 
-	$( '#ctc-get-coordinates-button' ).click( function() {
+		$( '#ctc-get-coordinates-button' ).click( function() {
 
-		var address, error;
+			var address;
 
-		// Address entered?
-		address = $('textarea[id^="ctmb-input-_ctc_"][id$="_address"]').val().trim();
-		if ( address ) {
+			// Get address
+			address = $('textarea[id^="ctmb-input-_ctc_"][id$="_address"]').val();
+			address = $("<div/>").html( address ).text(); // remove HTML
+			address = address.replace( /\r?\n/g, ', ' ); // replace line breaks with commas
+			address = address.trim(); // remove whitespace
 
-			// Convert address to one line
+			// Have address
+			if ( address ) {
 
+				// Geocode address via Google Maps API
+				$.ajax( {
+					url: 'http://maps.google.com/maps/api/geocode/json',
+					data: { address: address },
+					dataType: 'json',
+				} )
 
-			// Get coordinates from Google Maps geocoder
+				// Request succeeded
+				.done( function( data ) {
 
+					// Found address
+					if ( 'OK' == data.status ) {
 
-			// Failed to get coordinates
-			//error = '<?php echo esc_js( $error_failed ); ?>';
+						// Set values on inputs
+						$('input[id^="ctmb-input-_ctc_"][id$="_map_lat"]').val( data.results[0].geometry.location.lat );
+						$('input[id^="ctmb-input-_ctc_"][id$="_map_lng"]').val( data.results[0].geometry.location.lng );
 
-		}
+					}
 
-		// No address
-		else {
-			error = '<?php echo esc_js( $error_no_address ); ?>';
-		}
+					// Could not find address
+					else {
+						alert( '<?php echo esc_js( $error_failed ); ?>' );
+					}
 
-		// Show error
-		if ( error ) {
-			alert( error );
-		}
+				} )
+
+				// Request failed
+				.fail( function() {
+					alert( '<?php echo esc_js( $error_failed ); ?>' );
+				} )
+			}
+
+			// No address entered
+			else {
+				alert( '<?php echo esc_js( $error_no_address ); ?>' );
+			}
+
+		} );
 
 	} );
 
-} );
+	</script>
 
-</script>
-
-<?php
+	<?php
 
 }
 
