aeae75dca903fbc0da8a2dba24d13175bf8ff2ed
Steven Gliebe <work@stevengliebe.com>
2016-07-06 12:52:40 -0500

    Add ctc-map-field class to lat, lng, zoom and type for detecting changes

--- includes/admin/admin-event-fields.php
+++ includes/admin/admin-event-fields.php
@@ -424,7 +424,7 @@ function ctc_add_meta_box_event_location() {
 				'no_empty'			=> false, // if user empties value, force default to be saved instead
 				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
 				'attributes'		=> array(), // attr => value array (e.g. set min/max for number type)
-				'class'				=> 'ctmb-medium', // class(es) to add to input (try ctmb-medium, ctmb-small, ctmb-tiny)
+				'class'				=> 'ctmb-medium ctc-map-field', // class(es) to add to input (try ctmb-medium, ctmb-small, ctmb-tiny)
 				'field_attributes'	=> array(), // attr => value array for field container
 				'field_class'		=> '', // class(es) to add to field container
 				'custom_sanitize'	=> '', // function to do additional sanitization
@@ -452,7 +452,7 @@ function ctc_add_meta_box_event_location() {
 				'no_empty'			=> false, // if user empties value, force default to be saved instead
 				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
 				'attributes'		=> array(), // attr => value array (e.g. set min/max for number type)
-				'class'				=> 'ctmb-medium', // class(es) to add to input (try ctmb-medium, ctmb-small, ctmb-tiny)
+				'class'				=> 'ctmb-medium ctc-map-field', // class(es) to add to input (try ctmb-medium, ctmb-small, ctmb-tiny)
 				'field_attributes'	=> array(), // attr => value array for field container
 				'field_class'		=> '', // class(es) to add to field container
 				'custom_sanitize'	=> '', // function to do additional sanitization
@@ -476,7 +476,7 @@ function ctc_add_meta_box_event_location() {
 				'no_empty'			=> true, // if user empties value, force default to be saved instead
 				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
 				'attributes'		=> array(), // attr => value array (e.g. set min/max for number type)
-				'class'				=> '', // class(es) to add to input (try ctmb-medium, ctmb-small, ctmb-tiny)
+				'class'				=> 'ctc-map-field', // class(es) to add to input (try ctmb-medium, ctmb-small, ctmb-tiny)
 				'field_attributes'	=> array(), // attr => value array for field container
 				'field_class'		=> '', // class(es) to add to field container
 				'custom_sanitize'	=> '', // function to do additional sanitization
@@ -500,7 +500,7 @@ function ctc_add_meta_box_event_location() {
 				'no_empty'			=> true, // if user empties value, force default to be saved instead
 				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
 				'attributes'		=> array(), // attr => value array (e.g. set min/max for number type)
-				'class'				=> '', // class(es) to add to input (try ctmb-medium, ctmb-small, ctmb-tiny)
+				'class'				=> 'ctc-map-field', // class(es) to add to input (try ctmb-medium, ctmb-small, ctmb-tiny)
 				'field_attributes'	=> array(), // attr => value array for field container
 				'field_class'		=> '', // class(es) to add to field container
 				'custom_sanitize'	=> '', // function to do additional sanitization
--- js/map-after-fields.js
+++ js/map-after-fields.js
@@ -4,23 +4,42 @@
 
 jQuery( document ).ready( function( $ ) {
 
+	// Only if map container exists
+	if ( ! $( '#ctc-map-after-fields' ).length ) {
+		return;
+	}
+
 	/**************************************
 	 * SHOW MAP
 	 **************************************/
 
-	// Map container exists
-	if ( $( '#ctc-map-after-fields' ).length ) {
+	// REPLACE THIS WITH ACTUAL COORDS, ETC.
+	// NEED TO LOCALIZE FOR DEFAULT TYPE AND ZOOM
 
-		var map;
+	var map;
 
-		map = new google.maps.Map( document.getElementById( 'ctc-map-after-fields' ), {
-			center: {
-				lat: -34.397,
-				lng: 150.644,
-			},
-			zoom: 8,
-		} );
+	map = new google.maps.Map( document.getElementById( 'ctc-map-after-fields' ), {
+		center: {
+			lat: -34.397,
+			lng: 150.644,
+		},
+		zoom: 8,
+	} );
 
-	}
+	/**************************************
+	 * CHANGE MAP (based on field changes)
+	 **************************************/
+
+	$( '.ctc-map-field' ).bind( 'change', function() {
+
+		console.log( 'fields changed, update map' );
+
+	} );
+
+	/**************************************
+	 * CHANGE FIELDS (based on map changes)
+	 **************************************/
+
+
 
 } );
