6061a646aaf2d8fa7cf3d9dab7dd6d8769108752
Steven Gliebe <work@stevengliebe.com>
2017-11-01 14:22:50 -0500

    Tidy up ctc_update_recurring_event_dates()

--- includes/schedule.php
+++ includes/schedule.php
@@ -28,9 +28,9 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 function ctc_schedule_recurring_events() {
 
-	// Schedule if not already scheduled
+	// Schedule if not already scheduled.
 	if ( ! wp_next_scheduled( 'ctc_update_recurring_event_dates' ) ) {
-		wp_schedule_event( time(), 'hourly', 'ctc_update_recurring_event_dates' ); // hourly so happens as soon as possible after event ends the day before
+		wp_schedule_event( time(), 'hourly', 'ctc_update_recurring_event_dates' ); // hourly so happens as soon as possible after event ends the day before.
 	}
 
 }
@@ -38,86 +38,86 @@ function ctc_schedule_recurring_events() {
 add_action( 'wp', 'ctc_schedule_recurring_events' );
 
 /**
- * Update recurring event dates
+ * Update recurring event dates.
  *
  * @since 0.9
  */
 function ctc_update_recurring_event_dates() {
 
-	// Get all events with end date in past and have valid recurring value
+	// Get all events with end date in past and have valid recurring value.
 	$events_query = new WP_Query( array(
-		'post_type'	=> 'ctc_event',
-		'nopaging'	=> true,
-		'meta_query' => array(
-			'relation' => 'AND',
+		'post_type'       => 'ctc_event',
+		'nopaging'        => true,
+		'meta_query'      => array(
+			'relation'    => 'AND',
 			array(
-				'key' => '_ctc_event_end_date',
-				'value' => date_i18n( 'Y-m-d' ), // today localized
-		 		'compare' => '<', // earlier than today
-				'type' => 'DATE',
-		   ),
+				'key'     => '_ctc_event_end_date',
+				'value'   => date_i18n( 'Y-m-d' ), // today localized.
+				'compare' => '<', // earlier than today.
+				'type'    => 'DATE',
+			),
 			array(
-				'key' => '_ctc_event_recurrence',
-				'value' => array( 'weekly', 'monthly', 'yearly' ),
-		 		'compare' => 'IN',
-		   )
-		)
+				'key'     => '_ctc_event_recurrence',
+				'value'   => array( 'weekly', 'monthly', 'yearly' ),
+				'compare' => 'IN',
+			),
+		),
 	) );
 
-	// Loop events
+	// Loop events.
 	if ( ! empty( $events_query->posts ) ) {
 
-		// Instantiate recurrence class
+		// Instantiate recurrence class.
 		$ctc_recurrence = new CT_Recurrence();
 
-		// Loop events to modify dates
+		// Loop events to modify dates.
 		foreach ( $events_query->posts as $post ) {
 
-			// Get start and end date
+			// Get start and end date.
 			$start_date = get_post_meta( $post->ID, '_ctc_event_start_date', true );
 			$end_date = get_post_meta( $post->ID, '_ctc_event_end_date', true );
 
-		 	// Get recurrence
-		 	$recurrence = get_post_meta( $post->ID, '_ctc_event_recurrence', true );
+			// Get recurrence.
+			$recurrence = get_post_meta( $post->ID, '_ctc_event_recurrence', true );
 			$recurrence_end_date = get_post_meta( $post->ID, '_ctc_event_recurrence_end_date', true );
 
-			// Difference between start and end date in seconds
+			// Difference between start and end date in seconds.
 			$time_difference = strtotime( $end_date ) - strtotime( $start_date );
 
-			// Get soonest occurence that is today or later
+			// Get soonest occurrence that is today or later.
 			$args = array(
-				'start_date'			=> $start_date, // first day of event, YYYY-mm-dd (ie. 2015-07-20 for July 15, 2015)
-				'frequency'				=> $recurrence, // weekly, monthly, yearly
+				'start_date' => $start_date, // first day of event, YYYY-mm-dd (ie. 2015-07-20 for July 15, 2015).
+				'frequency'  => $recurrence, // weekly, monthly, yearly.
 			);
-			$args = apply_filters( 'ctc_event_recurrence_args', $args, $post ); // Custom Recurring Events add-on uses this
+			$args = apply_filters( 'ctc_event_recurrence_args', $args, $post ); // Pro / Custom Recurring Events add-ons hook into this.
 			$new_start_date = $ctc_recurrence->calc_next_future_date( $args );
 
-			// If no new start date gotten, set it to current start date
+			// If no new start date gotten, set it to current start date.
 			// This could be because recurrence ended, arguments are invalid, etc.
 			if ( ! $new_start_date ) {
 				$new_start_date = $start_date;
 			}
 
-			// Add difference between original start/end date to new start date to get new end date
+			// Add difference between original start/end date to new start date to get new end date.
 			$new_end_date = date( 'Y-m-d', ( strtotime( $new_start_date ) + $time_difference ) );
 
 			// Has recurrence ended?
-			// Recurrence end date exists and is earlier than new start date
+			// Recurrence end date exists and is earlier than new start date.
 			if ( $recurrence_end_date && strtotime( $recurrence_end_date ) < strtotime( $new_start_date ) ) {
 
-				// Unset recurrence option to keep dates from being moved forward
+				// Unset recurrence option to keep dates from being moved forward.
 				update_post_meta( $post->ID, '_ctc_event_recurrence', 'none' );
 
 			}
 
-			// No recurrence or recurrence end date is still future
+			// No recurrence or recurrence end date is still future.
 			else {
 
-				// Update start and end dates
+				// Update start and end dates.
 				update_post_meta( $post->ID, '_ctc_event_start_date', $new_start_date );
 				update_post_meta( $post->ID, '_ctc_event_end_date', $new_end_date );
 
-				// Update the hidden datetime fields for ordering
+				// Update the hidden datetime fields for ordering.
 				ctc_update_event_date_time( $post->ID );
 
 			}
