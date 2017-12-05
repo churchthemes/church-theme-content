d30a8ac5f60c69f6f9b62874dfffa744192e88c2
Steven Gliebe <work@stevengliebe.com>
2017-12-05 11:40:10 -0600

    Change settings descriptions and tabs to prepare for Pro

--- includes/settings.php
+++ includes/settings.php
@@ -32,35 +32,86 @@ function ctc_settings_setup() {
 
 	global $ctc_settings;
 
-	// Configuration
+	// Note to add to page description for Pro add-on when not active.
+	$pro_upgrade_desc_note = '';
+	if ( ! defined( 'CCP_VERSION' ) ) { // plugin not active.
+		$pro_upgrade_desc_note = sprintf(
+			/* translators: %1$s is URL for Church Content Pro info. */
+			__( '<a href="%1$s" target="_blank">Upgrade to Pro</a> to enable extra settings and features.', 'church-theme-content' ),
+			'https://churchthemes.com/plugins/church-content-pro/?utm_source=ctc&utm_medium=plugin&utm_campaign=church_content_pro&utm_content=settings'
+		);
+	}
+
+	// Configuration.
 	$config = array(
 
-		// Master Option
-		// All settings will be saved as an array under this single option ID
-		'option_id'		=> 'ctc_settings',
+		// Master Option.
+		// All settings will be saved as an array under this single option ID.
+		'option_id' => 'ctc_settings',
 
-		// Titles
-		'page_title' 	=> sprintf( __( '%1$s Settings', 'church-theme-content' ), CTC_NAME ),
-		'menu_title'	=> CTC_NAME,
+		// Titles.
+		'page_title' => sprintf(
+			/* translators: %1$s is Church Content plugin URL, %2$s is add-ons URL */
+			__( '%1$s Settings', 'church-theme-content' ),
+			CTC_NAME
+		),
+		'menu_title' => CTC_NAME,
 
-		// Settings page description
-						/* translators: %1$s is Church Content plugin URL, %2$s is add-ons URL */
-		'desc'			=> sprintf(
-							__( 'These settings are for the <a href="%1$s" target="_blank">Church Content</a> plugin and its <a href="%2$s" target="_blank">Add-ons</a>.', 'church-theme-content' ),
-							'https://churchthemes.com/plugins/church-content/?utm_source=ctc&utm_medium=plugin&utm_campaign=church-theme-content&utm_content=settings',
-							'https://churchthemes.com/plugins/?utm_source=ctc&utm_medium=plugin&utm_campaign=add-ons&utm_content=settings'
-						),
+		// Settings page description.
+		'desc' => sprintf(
+			/* translators: %1$s is Church Content plugin URL, %2$s is note about upgrading to Pro (if not active), %3$s is URL to Customizer. */
+			__( 'These settings are for the <a href="%1$s" target="_blank">Church Content</a> plugin. %2$s See the <a href="%3$s">Customizer</a> for theme-provided appearance settings.', 'church-theme-content' ),
+			'https://churchthemes.com/plugins/church-content/?utm_source=ctc&utm_medium=plugin&utm_campaign=church-theme-content&utm_content=settings',
+			$pro_upgrade_desc_note,
+			esc_url( admin_url( 'customize.php' ) )
+		),
 
 		// Plugin File
-		'plugin_file'	=> CTC_FILE,	// path to plugin's main file
+		'plugin_file'   => CTC_FILE,	// path to plugin's main file
 
 		// URL for CT Plugin Settings directory
 		// This is used for loading its CSS and JS files
 		'url'			=> CTC_URL . '/' . CTC_LIB_DIR . '/ct-plugin-settings',
 
-		// Section Tabs
+		// Section Tabs.
 		'sections' => array(
 
+			// Licenses.
+			'licenses' => array(
+
+				// Title.
+				'title'	=> _x( 'Licenses', 'settings', 'church-theme-content' ),
+
+				// Description.
+				'desc'	=> sprintf(
+								/* translators: %1$s is URL to Add-ons */
+								__( 'Save then activate your add-on license key to enable one-click updates.', 'church-theme-content' ),
+								'https://churchthemes.com/plugins/?utm_source=ctc&utm_medium=plugin&utm_campaign=add-ons&utm_content=settings'
+							),
+
+				// Fields (Settings).
+				'fields' => array(
+
+					// Example
+					/*
+					'setting_key' => array(
+						'name'				=> __( 'Field Name', 'church-theme-content' ),
+						'desc'				=> __( 'This is the description below the field.', 'church-theme-content' ),
+						'type'				=> 'text', // text, textarea, checkbox, checkbox_multiple, radio, select, number
+						'checkbox_label'	=> '', //show text after checkbox
+						'options'			=> array(), // array of keys/values for radio or select
+						'default'			=> '', // value to pre-populate option with (before first save or on reset)
+						'no_empty'			=> false, // if user empties value, force default to be saved instead
+						'allow_html'		=> false, // allow HTML to be used in the value
+						'class'				=> '', // classes to add to input
+						'custom_sanitize'	=> '', // function to do additional sanitization
+						'custom_content'	=> '', // function for custom display of field input
+					),
+					*/
+
+				)
+
+			),
 
 			// General
 			'general' => array(
@@ -92,47 +143,6 @@ function ctc_settings_setup() {
 						'custom_sanitize'	=> '', // function to do additional sanitization
 						'custom_content'	=> '', // function for custom display of field input
 					),
-
-				)
-
-			),
-
-			// Future
-			// - Possibly add extendable "Add-on Settings" with section for each add-on, like EDD
-			// - It would sit next to the existing "Add-on Licenses" tab, like EDD
-
-			// Add-on Licenses
-			'licenses' => array(
-
-				// Title
-				'title'	=> _x( 'Add-on Licenses', 'settings', 'church-theme-content' ),
-
-				// Description
-				'desc'	=> sprintf(
-								/* translators: %1$s is URL to Add-ons */
-								__( 'Save then activate your add-on license keys to enable one-click updates for them.', 'church-theme-content' ),
-								'https://churchthemes.com/plugins/?utm_source=ctc&utm_medium=plugin&utm_campaign=add-ons&utm_content=settings'
-							),
-
-				// Fields (Settings)
-				'fields' => array(
-
-					// Example
-					/*
-					'setting_key' => array(
-						'name'				=> __( 'Field Name', 'church-theme-content' ),
-						'desc'				=> __( 'This is the description below the field.', 'church-theme-content' ),
-						'type'				=> 'text', // text, textarea, checkbox, checkbox_multiple, radio, select, number
-						'checkbox_label'	=> '', //show text after checkbox
-						'options'			=> array(), // array of keys/values for radio or select
-						'default'			=> '', // value to pre-populate option with (before first save or on reset)
-						'no_empty'			=> false, // if user empties value, force default to be saved instead
-						'allow_html'		=> false, // allow HTML to be used in the value
-						'class'				=> '', // classes to add to input
-						'custom_sanitize'	=> '', // function to do additional sanitization
-						'custom_content'	=> '', // function for custom display of field input
-					),
-					*/
 
 				)
 
