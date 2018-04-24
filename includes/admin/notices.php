be7f06b45c554079fcd829cda5db0dea2ee5d018
Steven Gliebe <work@stevengliebe.com>
2018-04-24 12:02:12 -0500

    Add ctc_recurrence_php_notice_content() and use in ctc_recurrence_php_notice()

--- includes/admin/notices.php
+++ includes/admin/notices.php
@@ -18,6 +18,79 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  **********************************/
 
 /**
+ * Content for PHP update notice when CT Recurrence class needs it.
+ *
+ * This is shown in admin notice via ctc_recurrence_php_notice() and description
+ * for recurrence field in Gutenberg since admin_notices does not work there.
+ *
+ * @since 1.9
+ */
+function ctc_recurrence_php_notice_content() {
+
+	// Instantiate class.
+	$ct_recurrence = new CT_Recurrence();
+
+	// Show by default.
+	$show = true;
+
+	// Show notice only if PHP version is insufficient.
+	if ( ! $ct_recurrence->php_is_old ) {
+		$show = false;
+	}
+
+	// Show notice only if recurrence is enabled.
+	if ( ! ctc_field_supported( 'events', '_ctc_event_recurrence' ) ) {
+		$show = false;
+	}
+
+	// Show notice only on relevant screens.
+	// Dashboard, events list and event add/edit.
+	$screen = get_current_screen();
+	if ( 'dashboard' !== $screen->base && 'ctc_event' !== $screen->post_type ) {
+		$show = false;
+	}
+
+	// Show notice.
+	if ( $show ) {
+
+		// Build notice.
+		ob_start();
+		?>
+		<div id="ctc-recurrence-php-notice" class="notice notice-error is-dismissible">
+			<p>
+				<?php
+				printf(
+					wp_kses(
+						/* translators: %1$s is minimum required version of PHP, %2$s is URL with information on updating PHP. */
+						__( '<strong>Event recurrence is disabled.</strong> PHP %1$s or newer is required for recurrence to work. <a href="%2$s" target="_blank">Update PHP</a> to resolve.', 'church-theme-content' ),
+						array(
+							'strong' => array(),
+							'a' => array(
+								'href' => array(),
+								'target' => array(),
+							),
+						)
+					),
+					esc_html( $ct_recurrence->php_min_version ),
+					esc_url( ctc_ctcom_url( 'update-php', array( 'utm_content' => 'recurrence' ) ) )
+				);
+				?>
+			</p>
+		</div>
+
+		<?php
+		// Get content.
+		$content = ob_get_contents();
+		ob_end_clean();
+
+	}
+
+	// Return.
+	return apply_filters( '', $content );
+
+}
+
+/**
  * Show PHP update notice when CT Recurrence class needs it.
  *
  * CT Recurrence methods return empty when PHP version not satisfied.
@@ -27,52 +100,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 function ctc_recurrence_php_notice() {
 
-	// Instantiate class.
-	$ct_recurrence = new CT_Recurrence();
+	// Get notice if it is to be shown.
+	$notice = ctc_recurrence_php_notice_content();
 
-	// Show notice only if PHP version is insufficient.
-	if ( ! $ct_recurrence->php_is_old ) {
-		return;
+	// Have notice.
+	if ( $notice ) {
+		echo $notice;
 	}
-
-	// Show notice only if recurrence is enabled.
-	if ( ! ctc_field_supported( 'events', '_ctc_event_recurrence' ) ) {
-		return;
-	}
-
-	// Show notice only on relevant screens.
-	// Dashboard, events list and event add/edit.
-	$screen = get_current_screen();
-	if ( 'dashboard' !== $screen->base && 'ctc_event' !== $screen->post_type ) {
-		return;
-	}
-
-	// Show notice.
-	?>
-
-	<div id="ctc-recurrence-php-notice" class="notice notice-error is-dismissible">
-		<p>
-			<?php
-			printf(
-				wp_kses(
-					/* translators: %1$s is minimum required version of PHP, %2$s is URL with information on updating PHP. */
-					__( '<strong>Event recurrence is disabled.</strong> PHP %1$s or newer is required for recurrence to work. <a href="%2$s" target="_blank">Update PHP</a> to resolve.', 'church-theme-content' ),
-					array(
-						'strong' => array(),
-						'a' => array(
-							'href' => array(),
-							'target' => array(),
-						),
-					)
-				),
-				esc_html( $ct_recurrence->php_min_version ),
-				esc_url( ctc_ctcom_url( 'update-php', array( 'utm_content' => 'recurrence' ) ) )
-			);
-			?>
-		</p>
-	</div>
-
-	<?php
 
 }
 
