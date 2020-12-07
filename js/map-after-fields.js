786aeb4e870c63db4399120ea54641862d025179
Steven Gliebe <3372396+stevengliebe@users.noreply.github.com>
2020-12-07 11:17:50 -0600

    👌 IMPROVE: Replace jQuery deprecated click with on click

--- js/map-after-fields.js
+++ js/map-after-fields.js
@@ -2,7 +2,7 @@
  * Map after fields on event and location screen
  */
 
-jQuery( document ).ready( function( $ ) {
+jQuery(document).ready(function ($) {
 
 	/**************************************
 	 * SHOW MAP
@@ -12,43 +12,43 @@ jQuery( document ).ready( function( $ ) {
 	ctc_show_map_after_fields();
 
 	// Latitude/longitude change or keyup
-	$( '.ctc-map-lat-field, .ctc-map-lng-field' ).on( 'change keyup', function() {
-		ctc_show_map_after_fields( 'coordinates' );
-	} );
+	$('.ctc-map-lat-field, .ctc-map-lng-field').on('change keyup', function () {
+		ctc_show_map_after_fields('coordinates');
+	});
 
 	// Latitude/longitude paste
-	$( '.ctc-map-lat-field, .ctc-map-lng-field' ).on( 'paste', function() {
+	$('.ctc-map-lat-field, .ctc-map-lng-field').on('paste', function () {
 
 		// Update map map after timeout
-		setTimeout( function() {
-			ctc_show_map_after_fields( 'coordinates' );
-		}, 250 );
+		setTimeout(function () {
+			ctc_show_map_after_fields('coordinates');
+		}, 250);
 
-	} );
+	});
 
 	// Type change
-	$( '.ctc-map-type-field' ).on( 'change', function() {
-		ctc_show_map_after_fields( 'type' );
-	} );
+	$('.ctc-map-type-field').on('change', function () {
+		ctc_show_map_after_fields('type');
+	});
 
 	// Zoom change
-	$( '.ctc-map-zoom-field' ).on( 'input', function() { // do it as user drags, not just on release (change)
-		ctc_show_map_after_fields( 'zoom' ); // zoom only so user can drag/zoom to manually click
-	} );
+	$('.ctc-map-zoom-field').on('input', function () { // do it as user drags, not just on release (change)
+		ctc_show_map_after_fields('zoom'); // zoom only so user can drag/zoom to manually click
+	});
 
 	/**************************************
 	 * GEOCODE ADDRESS
 	 **************************************/
 
 	// Click "Get From Address" button
-	$( '#ctc-get-coordinates-button' ).click( function() {
+	$('#ctc-get-coordinates-button').on('click', function () {
 
 		var address;
 
 		// Show alert if no API Key
-		if ( ! ctc_map_after_fields_data.has_api_key ) {
+		if (!ctc_map_after_fields_data.has_api_key) {
 
-			alert( ctc_map_after_fields_data.missing_key_message );
+			alert(ctc_map_after_fields_data.missing_key_message);
 
 			// Don't attempt geocoding
 			return;
@@ -56,31 +56,31 @@ jQuery( document ).ready( function( $ ) {
 		}
 
 		// Get address
-		address = $( '.ctc-address-field' ).val(); // event or location
-		address = $("<div/>").html( address ).text(); // remove HTML
-		address = address.replace( /\r?\n/g, ', ' ); // replace line breaks with commas
+		address = $('.ctc-address-field').val(); // event or location
+		address = $("<div/>").html(address).text(); // remove HTML
+		address = address.replace(/\r?\n/g, ', '); // replace line breaks with commas
 		address = address.trim(); // remove whitespace
 
 		// Have address
-		if ( address ) {
+		if (address) {
 
 			// Geocode address
-	 		geocoder = new google.maps.Geocoder();
-			geocoder.geocode( {
+			geocoder = new google.maps.Geocoder();
+			geocoder.geocode({
 				'address': address
-			}, function( results, status ) {
+			}, function (results, status) {
 
 				var coordinates;
 
 				// Success
-				if ( google.maps.GeocoderStatus.OK == status ) {
+				if (google.maps.GeocoderStatus.OK == status) {
 
 					// Update Latitude and Longitude fields
-					$( '.ctc-map-lat-field' ).val( results[0].geometry.location.lat );
-					$( '.ctc-map-lng-field' ).val( results[0].geometry.location.lng );
+					$('.ctc-map-lat-field').val(results[0].geometry.location.lat);
+					$('.ctc-map-lng-field').val(results[0].geometry.location.lng);
 
 					// Map not showing (new Add), show it based on fields
-					if ( ! $( '#ctc-map-after-fields-container' ).is( ':visible' ) ) {
+					if (!$('#ctc-map-after-fields-container').is(':visible')) {
 						ctc_show_map_after_fields();
 					}
 
@@ -91,8 +91,8 @@ jQuery( document ).ready( function( $ ) {
 						coordinates = results[0].geometry.location;
 
 						// Move marker and recenter
-						ctc_map_after_fields_marker.setPosition( coordinates );
-						ctc_map_after_fields.panTo( coordinates );
+						ctc_map_after_fields_marker.setPosition(coordinates);
+						ctc_map_after_fields.panTo(coordinates);
 
 					}
 
@@ -102,114 +102,114 @@ jQuery( document ).ready( function( $ ) {
 				else {
 
 					// Give instructions
-					alert( ctc_map_after_fields_data.get_from_address_failed );
+					alert(ctc_map_after_fields_data.get_from_address_failed);
 
 				}
 
-			} );
+			});
 
 		}
 
 		// No address, show alert
 		else {
-			alert( ctc_map_after_fields_data.missing_address );
+			alert(ctc_map_after_fields_data.missing_address);
 		}
 
-	} );
+	});
 
-} );
+});
 
 /**************************************
  * FUNCTIONS
  **************************************/
 
 // Show Map
-function ctc_show_map_after_fields( update ) {
+function ctc_show_map_after_fields(update) {
 
 	var lat, lng, zoom, type, coordinates, click_timeout;
 
 	// Only if map container exists
-	if ( ! jQuery( '#ctc-map-after-fields-container' ).length ) {
+	if (!jQuery('#ctc-map-after-fields-container').length) {
 		return;
 	}
 
 	// Get Coordinates
-	lat = parseFloat( jQuery( '.ctc-map-lat-field' ).val() );
-	lng = parseFloat( jQuery( '.ctc-map-lng-field' ).val() );
+	lat = parseFloat(jQuery('.ctc-map-lat-field').val());
+	lng = parseFloat(jQuery('.ctc-map-lng-field').val());
 
 	// Get Type
 	type = 'ROAD'; // default if no type field
-	if ( jQuery( '.ctc-map-type-field' ).length ) { // field supported
-		type = jQuery( '.ctc-map-type-field:checked' ).val();
+	if (jQuery('.ctc-map-type-field').length) { // field supported
+		type = jQuery('.ctc-map-type-field:checked').val();
 	}
 
 	// Get Zoom
 	zoom = 14; // default if no zoom field
-	if ( jQuery( '.ctc-map-zoom-field' ).length ) { // field supported
-		zoom = parseFloat( jQuery( '.ctc-map-zoom-field' ).val() );
+	if (jQuery('.ctc-map-zoom-field').length) { // field supported
+		zoom = parseFloat(jQuery('.ctc-map-zoom-field').val());
 	}
 
 	// Latitude and Longitude entered
-	if ( lat && lng ) {
+	if (lat && lng) {
 
 		// Coordinates
 		coordinates = { lat: lat, lng: lng };
 
 		// Map not showing (new Add), show it based on fields
-		if ( ! jQuery( '#ctc-map-after-fields-container' ).is( ':visible' ) ) {
+		if (!jQuery('#ctc-map-after-fields-container').is(':visible')) {
 
 			// Show map container
 			// Show container first because hidden div has no size
 			// Doing this first prevents empty gray map in some cases
-			jQuery( '#ctc-map-after-fields-container' ).show();
+			jQuery('#ctc-map-after-fields-container').show();
 
 			// Render map first time
-			ctc_map_after_fields = new google.maps.Map( document.getElementById( 'ctc-map-after-fields' ), {
+			ctc_map_after_fields = new google.maps.Map(document.getElementById('ctc-map-after-fields'), {
 				center: coordinates,
 				zoom: zoom,
 				mapTypeId: google.maps.MapTypeId[type],
 				disableDefaultUI: true, // form fields control zoom, type, etc.
 				scrollwheel: false, // disable scroll zoom (mistake prone, let use Zoom field)
 				clickableIcons: false, // place names interfere with manual location clicking
-			} );
+			});
 
 			// Add marker to new map
-			ctc_map_after_fields_marker = new google.maps.Marker( {
+			ctc_map_after_fields_marker = new google.maps.Marker({
 				position: coordinates,
 				map: ctc_map_after_fields,
 			});
 
 			// Move marker to point clicked
 			click_timeout = null;
-			ctc_map_after_fields.addListener( 'click', function( event ) {
+			ctc_map_after_fields.addListener('click', function (event) {
 
-    			click_timeout = setTimeout(function() {
+				click_timeout = setTimeout(function () {
 
 					// Move marker
-					ctc_map_after_fields_marker.setPosition( event.latLng );
+					ctc_map_after_fields_marker.setPosition(event.latLng);
 
 					// Center on marker
-					setTimeout( function() {
-						ctc_map_after_fields.panTo( event.latLng );
-					}, 200 );
+					setTimeout(function () {
+						ctc_map_after_fields.panTo(event.latLng);
+					}, 200);
 
 					// Update Latitude/Longitude fields
-					jQuery( '.ctc-map-lat-field' ).val( event.latLng.lat() );
-					jQuery( '.ctc-map-lng-field' ).val( event.latLng.lng() );
+					jQuery('.ctc-map-lat-field').val(event.latLng.lat());
+					jQuery('.ctc-map-lng-field').val(event.latLng.lng());
 
-			    }, 200);
+				}, 200);
 
-			} );
+			});
 
-				// Prevent single click from being triggered on double click
-				ctc_map_after_fields.addListener( 'dblclick', function() {
-					clearTimeout( click_timeout );
-				} );
+			// Prevent single click from being triggered on double click
+			ctc_map_after_fields.addListener('dblclick', function () {
+				clearTimeout(click_timeout);
+			});
 
 			// Make double click to zoom update Zoom field
-			ctc_map_after_fields.addListener( 'zoom_changed', function() {
-				jQuery( '.ctc-map-zoom-field' ).val( ctc_map_after_fields.getZoom() );
-			} );
+			ctc_map_after_fields.addListener('zoom_changed', function () {
+				jQuery('.ctc-map-zoom-field').val(ctc_map_after_fields.getZoom());
+			});
 
 		}
 
@@ -217,19 +217,19 @@ function ctc_show_map_after_fields( update ) {
 		else {
 
 			// Adjust type
-			if ( update == 'type' || ! update ) {
-				ctc_map_after_fields.setMapTypeId( google.maps.MapTypeId[type] );
+			if (update == 'type' || !update) {
+				ctc_map_after_fields.setMapTypeId(google.maps.MapTypeId[type]);
 			}
 
 			// Adjust zoom
-			if ( update == 'zoom' || ! update ) {
-				ctc_map_after_fields.setZoom( zoom );
+			if (update == 'zoom' || !update) {
+				ctc_map_after_fields.setZoom(zoom);
 			}
 
 			// Move marker and recenter
-			if ( update == 'coordinates' || ! update ) {
-				ctc_map_after_fields_marker.setPosition( coordinates );
-				ctc_map_after_fields.panTo( coordinates );
+			if (update == 'coordinates' || !update) {
+				ctc_map_after_fields_marker.setPosition(coordinates);
+				ctc_map_after_fields.panTo(coordinates);
 			}
 
 		}
@@ -238,7 +238,7 @@ function ctc_show_map_after_fields( update ) {
 
 	// Hide map container if no lat/lng
 	else {
-		jQuery( '#ctc-map-after-fields-container' ).hide();
+		jQuery('#ctc-map-after-fields-container').hide();
 	}
 
 }
--- js/settings.js
+++ js/settings.js
@@ -2,93 +2,93 @@
  * Plugin Settings
  */
 
-jQuery( document ).ready( function( $ ) {
+jQuery(document).ready(function ($) {
 
 	/**************************************
 	 * PRO DISABLED
 	 **************************************/
 
 	// Add .ctc-pro-setting-inactive and .button-disabled to image button when image input readonly.
-	if ( $( '#ctps-field-podcast_image' ).hasClass( 'ctc-setting-readonly' ) ) {
-		$( '.ctps-upload-file', $( '#ctps-field-podcast_image' ).parent( '.ctps-section' ) ).addClass( 'ctc-pro-setting-inactive button-disabled' );
+	if ($('#ctps-field-podcast_image').hasClass('ctc-setting-readonly')) {
+		$('.ctps-upload-file', $('#ctps-field-podcast_image').parent('.ctps-section')).addClass('ctc-pro-setting-inactive button-disabled');
 	}
 
 	// Show notice when user engages field requiring Pro while Pro is inactive.
 	// The Pro fields have readonly attribute so cannot be changed, but will be saved.
-	$( '.ctc-pro-setting-inactive' ).on( 'focus, click', function( e ) {
+	$('.ctc-pro-setting-inactive').on('focus, click', function (e) {
 
 		// Prevent clicks on links from having effect.
 		e.preventDefault();
 
 		// Remove previous instance of message before showing new.
-		$( '.ctc-pro-setting-inactive-message-inline' ).remove();
+		$('.ctc-pro-setting-inactive-message-inline').remove();
 
 		// Get parent cell.
-		var $field_container = $( this ).parents( 'td' );
+		var $field_container = $(this).parents('td');
 
 		// Get message from section description.
-		var $message = $( '.ctc-pro-setting-inactive-message:visible' ).html();
+		var $message = $('.ctc-pro-setting-inactive-message:visible').html();
 
 		// Have message.
-		if ( $message ) {
+		if ($message) {
 
 			// Copy message below field.
-			$field_container.append( '<span class="ctc-pro-setting-inactive-message-inline">' + $message + '</span>' );
+			$field_container.append('<span class="ctc-pro-setting-inactive-message-inline">' + $message + '</span>');
 
 			// Fade it in.
-			$( '.ctc-pro-setting-inactive-message-inline' ).hide().fadeIn( 'fast' );
+			$('.ctc-pro-setting-inactive-message-inline').hide().fadeIn('fast');
 
 		}
 
-	} );
+	});
 
 	// Prevent checkbox/radio changes on inactive fields (due to missing theme support or Pro being required).
 	// readonly attribute does not stop changes to checkbox states.
-	$( 'input[type=checkbox].ctc-setting-readonly, input[type=radio].ctc-setting-readonly' ).click( function( e ) {
+	$('input[type=checkbox].ctc-setting-readonly, input[type=radio].ctc-setting-readonly').on('click', function (e) {
 		return false;
-	} );
+	});
 
 	/**************************************
 	 * PODCAST
 	 **************************************/
 
 	// Open Podcast section when "Podcast Settings" link clicked in Sermons section.
-	$( '.ctps-field-podcast_content a' ).click( function( e ) {
+	$('.ctps-field-podcast_content a').on('click', function (e) {
 
 		// Prevent regular click action.
 		e.preventDefault();
 
 		// Switch to Podcast tab.
-		ctps_switch_section( 'podcast' );
+		ctps_switch_section('podcast');
 
-	} );
+	});
 
 	// Copy Feed URL to clipboard.
-	var clipboard = new ClipboardJS( '#ctc-copy-podcast-url-button', {
+	var clipboard = new ClipboardJS('#ctc-copy-podcast-url-button', {
 
 		// Get URL.
-		text: function( trigger ) {
-			return $( '#ctc-settings-podcast-feed-link' ).attr( 'href' );
+		text: function (trigger) {
+			return $('#ctc-settings-podcast-feed-link').attr('href');
 		}
 
-	} ).on( 'success', function( e ) {
+	}).on('success', function (e) {
 
 		// Show message.
-	    $( '#ctc-podcast-url-copied' ).fadeIn( 'fast' );
+		$('#ctc-podcast-url-copied').fadeIn('fast');
 
-	    // Hide message.
-	    setTimeout( function() {
-	    	$( '#ctc-podcast-url-copied' ).fadeOut( 'fast' );
-	    }, 3000 );
+		// Hide message.
+		setTimeout(function () {
+			$('#ctc-podcast-url-copied').fadeOut('fast');
+		}, 3000);
 
 		// Stop click.
 		return false;
 
-	} );
+	});
 
-		// Stop click to # on Copy button.
-		$( '#ctc-copy-podcast-url-button' ).on( 'click', function( e ) {
-			e.preventDefault();
-		} );
+	// Stop click to # on Copy button.
+	$('#ctc-copy-podcast-url-button').on('click', function (e) {
+		e.preventDefault();
+	});
 
-} );
+});
